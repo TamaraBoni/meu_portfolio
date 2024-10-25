fad808ef726d15c6a5960307b56639a866eace46
Andenik <andenik@yahoo.com.br>
2024-10-25 03:58:14 -0300

    estruturada pagina para realizar ediao dos checkpoints

--- src/app/checkpoint/[id]/page.tsx
+++ src/app/checkpoint/[id]/page.tsx
@@ -1,34 +1,108 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+"use client";
+import { TipoCheckpoints } from "@/types/types";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-const CheckPointDetail = () => {
-  const router = useRouter();
-  const { id } = router.query; // Pega o ID da URL
-  const [checkPoint, setCheckPoint] = useState(null);
+export default function EditarCheckpoint({ params }: { params: { rm: number } }) {
+  const navigate = useRouter();
 
-  const fetchCheckPoint = async (id) => {
-    const response = await fetch(`/api/checkpoints?id=${id}`);
-    const data = await response.json();
-    setCheckPoint(data);
-  };
+  const [checkpoint, setCheckpoint] = useState<TipoCheckpoints>({
+    rm: 0,
+    nome: "",
+    nota: 0,
+    materia: "",
+    checkpoint: "",
+  });
 
   useEffect(() => {
-    if (id) {
-      fetchCheckPoint(id);
-    }
-  }, [id]);
+    const chamadaDaApi = async () => {
+      const response = await fetch(`http://localhost:3000/api/checkpoint-route/${params.rm}`);
+      const dados = await response.json();
+      setCheckpoint(dados);
+    };
+    chamadaDaApi();
+  }, [params.rm]);
 
-  if (!checkPoint) {
-    return <div>Carregando...</div>; // Estado de carregamento
-  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCheckpoint({ ...checkpoint, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3000/api/checkpoint-route/${params.rm}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(checkpoint),
+      });
+
+      if (response.ok) {
+        alert("Checkpoint alterado com sucesso");
+        // Reset no state
+        setCheckpoint({
+          rm: 0,
+          nome: "",
+          nota: 0,
+          materia: "",
+          checkpoint: "",
+        });
+        // Redirecionar para /checkpoints
+        navigate.push("/checkpoint");
+      }
+    } catch (error) {
+      console.error("Falha ao realizar a alteração: ", error);
+    }
+  };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">{checkPoint.titulo}</h1>
-      <p>Nota: {checkPoint.nota}</p>
-      <p>Feedback: {checkPoint.feedback}</p>
+    <div>
+      <h1>Editar Checkpoint</h1>
+
+      <div>
+        <form onSubmit={handleSubmit} className="form">
+          <div>
+            <label>Nome</label>
+            <input type="text" id="idNome" name="nome" value={checkpoint.nome} onChange={handleChange} placeholder="Nome do aluno" required />
+          </div>
+          <div>
+            <label>Nota</label>
+            <input
+              type="number"
+              id="idNota"
+              name="nota"
+              value={checkpoint.nota}
+              onChange={handleChange}
+              placeholder="Nota"
+              required
+              min={0}
+              max={10}
+            />
+          </div>
+          <div>
+            <label>Matéria</label>
+            <input type="text" id="idMateria" name="materia" value={checkpoint.materia} onChange={handleChange} placeholder="Matéria" required />
+          </div>
+          <div>
+            <label>Checkpoint</label>
+            <input
+              type="text"
+              id="idCheckpoint"
+              name="checkpoint"
+              value={checkpoint.checkpoint}
+              onChange={handleChange}
+              placeholder="Nome do checkpoint"
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Alterar</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-};
-
-export default CheckPointDetail;
+}
